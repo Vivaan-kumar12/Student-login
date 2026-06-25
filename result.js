eaa7deb4b28f76a443137ee0efbfb4fa96b3ced7
Vivaan-kumar12 <kumarvivan2024@gmail.com>
2026-06-25 10:20:30 +0530

    Add files via upload

---
+++ result.js
@@ -0,0 +1,376 @@
+if(sessionStorage.getItem("loggedIn") !== "true"){
+
+    alert(
+    "Session Expired. Please Login Again."
+    );
+
+    window.location.href = "index.html";
+
+}
+// Student Data
+
+const students = {
+  "304": {
+    name: "Shreya Kumari",
+    class: "1",
+    father: "Pankaj Kumar",
+    attendance: "95%",
+    results: {
+	     		"June 2026": [
+      ["English", 50, 17, 45],
+      ["Math", 50, 17, 43],
+      ["Hindi", 50, 17, 42],
+      ["Science", 50, 17, 44],
+      ["Social Studies", 50, 17, 46],
+      ["Computer", 50, 17, 48]
+        ]
+     }
+  },
+
+  "313": {
+    name: "Shristi Singh",
+    class: "1",
+    father: "Sumit Singh",
+    attendance: "92%",
+    results: {
+			"June 2026": [
+      ["English", 50, 17, 40],
+      ["Math", 50, 17, 41],
+      ["Hindi", 50, 17, 39],
+      ["Science", 50, 17, 42],
+      ["Social Studies", 50, 17, 43],
+      ["Computer", 50, 17, 44]
+         ]
+     }
+  },
+
+  "310": {
+    name: "Akansha Singh",
+    class: "2",
+    father: "Brijesh Rai",
+    attendance: "90%",
+    	results: {
+	  	"June 2026": [
+      ["English", 50, 17, 41],
+      ["Math", 50, 17, 43],
+      ["Hindi", 50, 17, 40],
+      ["Science", 50, 17, 44],
+      ["Social Studies", 50, 17, 42],
+      ["Computer", 50, 17, 45]
+         ]
+     }
+  },
+
+  "312": {
+    name: "Shreyansh Singh",
+    class: "3",
+    father: " ",
+    attendance: "89%",
+    results: {
+		"June 2026": [
+      ["English", 50, 17, 38],
+      ["Math", 50, 17, 40],
+      ["Hindi", 50, 17, 39],
+      ["Science", 50, 17, 41],
+      ["Social Studies", 50, 17, 42],
+      ["Computer", 50, 17, 44]
+        ]
+     }
+  },
+
+  "306": {
+    name: "Advik Singh",
+    class: "4",
+    father: "Pappu Singh",
+    attendance: "93%",
+    results: {
+		"June 2026": [
+      ["English", 60, 20, 50],
+      ["Math", 60, 20, 48],
+      ["Hindi", 60, 20, 49],
+      ["Science", 60, 20, 51],
+      ["Social Studies", 60, 20, 52]
+        ]
+     }
+  },
+
+  "315": {
+    name: "Aarnav Singh",
+    class: "4",
+    father: "Pappu Singh",
+    attendance: "91%",
+    results: {
+		"June 2026": [
+      ["English", 60, 20, 47],
+      ["Math", 60, 20, 46],
+      ["Hindi", 60, 20, 48],
+      ["Science", 60, 20, 49],
+      ["Social Studies", 60, 20, 50]
+         ]
+     }
+  },
+
+  "309": {
+    name: "Sonali Singh",
+    class: "4",
+    father: "Rajesh Singh",
+    attendance: "94%",
+    results: {
+	"June 2026": [
+      ["English", 60, 20, 25],
+      ["Math", 60, 20, 16],
+      ["Hindi", 60, 20, 52],
+      ["Science", 60, 20, 55],
+      ["Social Studies", 60, 20, 20]
+        ]
+     }
+  },
+
+  "311": {
+    name: "Ayush Kumar",
+    class: "5",
+    father: "Pankaj Kumar",
+    attendance: "88%",
+    results: {
+		"June 2026": [
+      ["English", 60, 20, 44],
+      ["Math", 60, 20, 45],
+      ["Hindi", 60, 20, 46],
+      ["Science", 60, 20, 47],
+      ["Social Studies", 60, 20, 48]
+           ]
+      }
+  },
+
+  "314": {
+    name: "Anshika Singh",
+    class: "6",
+    father: "Rajesh Kumar Singh",
+    attendance: "96%",
+    results: {
+		"June 2026": [
+      ["English", 60, 20, 58],
+      ["Math", 60, 20, 56],
+      ["Hindi", 60, 20, 57],
+      ["Science", 60, 20, 58],
+      ["Social Studies", 60, 20, 59]
+        ]
+     }
+  },
+}
+
+// Get Login Data
+
+const roll = localStorage.getItem("studentRoll");
+const month = localStorage.getItem("selectedMonth");
+console.log("Selected Month =", month);
+
+const student = students[roll];
+
+if (!student) {
+    alert("Student data not found");
+    window.location.href = "index.html";
+}
+
+console.log(student);
+console.log(month);
+console.log(student.results);
+const selectedResult = student.results[month];
+console.log(selectedResult);
+
+if (!selectedResult) {
+
+document.body.innerHTML = `
+<div style="
+text-align:center;
+padding:100px;
+font-family:Arial;
+">
+<h1>Result Not Uploaded Yet</h1>
+<p>Result for ${month} is not available.</p>
+</div>
+`;
+
+throw new Error("Result Not Uploaded");
+}
+
+// Profile Section
+
+document.getElementById("studentName").textContent = student.name;
+
+document.getElementById("studentRoll").textContent =
+  "Class " + student.class + " - A";
+
+document.getElementById("fatherName").textContent =
+  student.father;
+
+document.getElementById("month").textContent =
+  month;
+
+// Table
+
+let totalMarks = 0;
+let maxMarks = 0;
+
+const table = document.getElementById("marksTable");
+
+selectedResult.forEach(subject => {
+
+  const subjectName = subject[0];
+  const max = subject[1];
+  const pass = subject[2];
+  const marks = subject[3];
+
+  totalMarks += marks;
+  maxMarks += max;
+
+  const status =
+    marks >= pass ? "Pass" : "Fail";
+
+  table.innerHTML += `
+    <tr>
+      <td>${subjectName}</td>
+      <td>${max}</td>
+      <td>${pass}</td>
+      <td>${marks}</td>
+      <td>${status}</td>
+    </tr>
+  `;
+});
+
+// Percentage
+
+const percentage =
+((totalMarks / maxMarks) * 100)
+.toFixed(2);
+
+document.getElementById("totalMarks").textContent =
+totalMarks + " / " + maxMarks;
+
+document.getElementById("percentage").textContent =
+percentage + "%";
+
+// Grade & Division
+
+let grade = "";
+let division = "";
+
+if (percentage >= 90) {
+    grade = "A1";
+}
+else if (percentage >= 80) {
+    grade = "A2";
+}
+else if (percentage >= 70) {
+    grade = "B1";
+}
+else if (percentage >= 60) {
+    grade = "B2";
+}
+else if (percentage >= 50) {
+    grade = "C1";
+}
+else if (percentage >= 40) {
+    grade = "C2";
+}
+else if (percentage >= 33) {
+    grade = "D";
+}
+else {
+    grade = "F";
+}
+
+if (percentage >= 80) {
+    division = "1st";
+}
+else if (percentage >= 50) {
+    division = "2nd";
+}
+else if (percentage >= 33) {
+    division = "3rd";
+}
+else {
+    division = "Fail";
+}
+document.getElementById("grade").textContent =
+grade;
+
+document.getElementById("division").textContent =
+division;
+
+// Attendance
+
+document.getElementById("attendance").textContent =
+student.attendance;
+
+// Comment
+
+if (percentage >= 80) {
+
+  document.getElementById("comment").textContent =
+  "Excellent performance! Keep up the good work. Stay focused and aim higher.";
+
+} else {
+
+  document.getElementById("comment").textContent =
+  "Needs hard work and regular practice for better improvement.";
+}
+function printResult(){
+
+window.print();
+
+}
+
+function downloadPDF(){
+
+window.print();
+
+}
+
+function logoutStudent(){
+
+localStorage.clear();
+
+sessionStorage.clear();
+
+window.location.replace(
+"index.html"
+);
+
+}
+history.pushState(null,null,location.href);
+
+window.onpopstate = function(){
+
+    history.go(1);
+
+};
+
+function updateClock(){
+
+    const clock = document.getElementById("resultClock");
+
+    if(!clock){
+        return;
+    }
+
+    const now = new Date();
+
+    const time = now.toLocaleTimeString('en-IN');
+
+    const day = now.toLocaleDateString('en-IN',{
+        weekday:'long'
+    });
+
+    const date = now.toLocaleDateString('en-IN',{
+        day:'numeric',
+        month:'long',
+        year:'numeric'
+    });
+
+    clock.innerHTML =
+    `${time} | ${day} | ${date}`;
+}
+
+updateClock();
+setInterval(updateClock,1000);
---
+++ script.js
@@ -0,0 +1,190 @@
+const dropdown = document.getElementById("monthYear");
+
+const months = [
+"January","February","March","April",
+"May","June","July","August",
+"September","October","November","December"
+];
+
+for(let year=2026; year<=2027; year++){
+
+    for(let month=0; month<months.length; month++){
+
+        if(year===2026 && month<5){
+            continue;
+        }
+
+        let option = document.createElement("option");
+
+        option.value =
+        months[month] + " " + year;
+
+        option.textContent =
+        months[month] + " " + year;
+
+        dropdown.appendChild(option);
+    }
+}
+
+const students = [
+
+{
+name:"Shreya Kumari",
+roll:"304"
+},
+
+{
+name:"Shristi Singh",
+roll:"313"
+},
+
+{
+name:"Akansha Singh",
+roll:"310"
+},
+
+{
+name:"Shreyansh Singh",
+roll:"312"
+},
+
+{
+name:"Advik Singh",
+roll:"306"
+},
+
+{
+name:"Aarnav Singh",
+roll:"315"
+},
+
+{
+name:"Sonali Singh",
+roll:"309"
+},
+
+{
+name:"Ayush Kumar",
+roll:"311"
+},
+
+{
+name:"Anshika Singh",
+roll:"314"
+}
+
+];
+
+function loginStudent(){
+
+    const name =
+    document.getElementById("studentName")
+    .value
+    .trim();
+
+    const roll =
+    document.getElementById("rollNumber")
+    .value
+    .trim();
+
+    const month =
+    document.getElementById("monthYear")
+    .value;
+
+    if(
+        name==="" ||
+        roll==="" ||
+        month===""
+    ){
+
+        alert(
+            "Please fill all fields"
+        );
+
+        return;
+    }
+
+    const studentFound =
+    students.find(student =>
+
+        student.name.toLowerCase()
+        ===
+        name.toLowerCase()
+
+        &&
+
+        student.roll===roll
+    );
+
+    if(!studentFound){
+
+        alert(
+        "Invalid Student Name or Roll Number"
+        );
+
+        return;
+    }
+
+    localStorage.setItem(
+        "studentName",
+        studentFound.name
+    );
+
+    localStorage.setItem(
+        "studentRoll",
+        studentFound.roll
+    );
+
+    localStorage.setItem(
+    "selectedMonth",
+    month
+);
+
+sessionStorage.setItem("loggedIn","true");
+
+window.location.href = "result.html";
+
+}
+
+function updateClock() {
+
+    const now = new Date();
+
+    const optionsDate = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    };
+
+    const time = now.toLocaleTimeString('en-IN');
+
+    const date = now.toLocaleDateString(
+        'en-IN',
+        optionsDate
+    );
+
+    const text = `
+        ${time}
+        <br>
+        ${date}
+    `;
+
+    const loginClock =
+        document.getElementById("liveClock");
+
+    const resultClock =
+        document.getElementById("resultClock");
+
+    if(loginClock){
+        loginClock.innerHTML = text;
+    }
+
+    if(resultClock){
+        resultClock.innerHTML = text;
+    }
+}
+
+setInterval(updateClock,1000);
+
+updateClock();
