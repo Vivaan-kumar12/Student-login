a9a6f238c722a7cc70403b2bda0bf9cb89806117
Vivaan-kumar12 <kumarvivan2024@gmail.com>
2026-06-27 08:44:46 +0530

    Implement admin login functionality

--- admin.js
+++ admin.js
@@ -1,1 +1,29 @@
+function adminLogin(){
 
+const username =
+document.getElementById("username").value.trim();
+
+const password =
+document.getElementById("password").value.trim();
+
+if(
+username==="admin"
+&&
+password==="12345"
+){
+
+sessionStorage.setItem(
+"adminLoggedIn",
+"true"
+);
+
+alert("Login Successful");
+
+}
+else{
+
+alert("Invalid Username or Password");
+
+}
+
+}
