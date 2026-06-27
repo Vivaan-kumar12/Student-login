fb9686ac2a70f3e7f5d0ffc77257502339673fb3
Vivaan-kumar12 <kumarvivan2024@gmail.com>
2026-06-27 12:32:58 +0530

    Create Firebase.js

---
+++ Firebase.js
@@ -0,0 +1,16 @@
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDC0bSVvhqkRPZ8ivBBvGgkPYv7JBoazJk",
+  authDomain: "student-result-portal-d231f.firebaseapp.com",
+  projectId: "student-result-portal-d231f",
+  storageBucket: "student-result-portal-d231f.firebasestorage.app",
+  messagingSenderId: "321281975258",
+  appId: "1:321281975258:web:b4daaebec7af05435d96e3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export { app };
